1a6454324c998d26eeab2571308a39762bb7b8c6
dellclm1100 <275298711@qq.com>
2019-06-02 17:03:16 +0800

    应用路由

---
+++ src/router.js
@@ -0,0 +1,20 @@
+import Vue from "vue";
+import Router  from "vue-router";
+import home from "./views/home"
+import about from "./views/about"
+Vue.use(Router);
+export default new Router({
+    mode:'history',
+    routes:[
+        {
+            path:'/',
+            name:"home",
+            component:home
+        },
+        {
+            path:'/about',
+            name:'about',
+            component:about
+        }
+    ]
+})
